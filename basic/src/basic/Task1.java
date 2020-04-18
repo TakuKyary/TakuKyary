5195d6190b634b4b55f126002080f285cd5fd7d1
TakuKyary <m.tky.olv39@gmail.com>
2020-04-19 01:41:38 +0900

    課題提出 - Javaの基礎

--- basic/src/basic/Task1.java
+++ basic/src/basic/Task1.java
@@ -1,9 +1,8 @@
 package basic;
 
-public class Task1 {
+class Task1 {
 	public static void main(String args[]) {
-		System.out.print("初めての課題");
-		System.out.println();
+		System.out.println("初めての課題");
 		System.out.print("Java攻略の道");
 	}
 }
---
+++ basic/src/basic/Task2.java
@@ -0,0 +1,10 @@
+package basic;
+
+class Task2 {
+	public static void main(String args[]) {
+		//コメント
+		
+		System.out.println("hogehoge");
+		System.out.print("hogehoge2");
+	}
+}
